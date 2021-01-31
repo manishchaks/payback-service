f95c6d2c39b4922a7b978fd15cfc12811eb1314e
Manish Chakravarty <manish.chakravarty@jda.com>
2021-01-31 11:15:35 +0530

    Initial Spec and setup

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,3 +1,8 @@
 class Merchant
+  attr_accessor :name, :discount_percentage
 
+  def initialize(name, discount_percentage)
+    @name = name
+    @discount_percentage = discount_percentage
+  end
 end
--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -1,6 +1,12 @@
 require 'merchant.rb'
 describe Merchant do
-  describe "initialization" do
-    merchant = Merchant.new
+  context "Initialization" do
+    it "is possible to create a valid merchant object" do
+      merchant = Merchant.new('m1', 1.5)
+      expect(merchant).to respond_to(:name)
+      expect(merchant).to respond_to(:discount_percentage)
+      expect(merchant.name).to eq('m1')
+      expect(merchant.discount_percentage).to eq(1.5)
+    end
   end
 end
