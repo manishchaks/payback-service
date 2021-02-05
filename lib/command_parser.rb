2ee78dab357f805056aa6b7b688604ce477a772a
Manish Chakravarty <manish.chakravarty@jda.com>
2021-02-05 08:49:10 +0530

    Added more functionality and specs for transactions

--- lib/command_parser.rb
+++ lib/command_parser.rb
@@ -7,7 +7,7 @@ class CommandParser
   def parse(buf)
     p "parsing command"
     if buf.start_with?('new')
-      parse_new_command(buf)
+      return parse_new_command(buf)
     end
   end
 
@@ -18,9 +18,54 @@ class CommandParser
     #We know the first element is already 'new'
     if cmd_array[1] == 'user'
       user = User.new(cmd_array[2], cmd_array[3], cmd_array[4].to_i)
-
       @payback_service.onboard_user(user)
-      "#{user.name} (#{user.credit_limit})"
+      return "#{user.name} (#{user.credit_limit})"
+    end
+    if cmd_array[1] == 'merchant'
+      discount_percentage = cmd_array[4].delete_suffix('%').to_f
+      merchant = Merchant.new(cmd_array[2],cmd_array[3],discount_percentage)
+      @payback_service.onboard_merchant(merchant)
+      return "#{merchant.name} (#{merchant.discount_percentage}%)"
+    end
+    if cmd_array[1] == 'txn'
+      return transact(cmd_array)
     end
   end
+
+  private
+
+  def transact(cmd_array)
+    user = find_user_by_name(cmd_array[2])
+    return "User #{cmd_array[2]} not found" unless user
+
+    merchant = find_merchant_by_name(cmd_array[3])
+    return "Merchant #{cmd_array[3]} not found" unless merchant
+
+    amount = cmd_array[4].to_i
+    result = @payback_service.transact(user, merchant, amount)
+    if result
+      return 'success!'
+    else
+      return 'rejected! (reason: credit limit)'
+    end
+  end
+
+  def find_user_by_name(username)
+    @payback_service.users.each do |user|
+      if user.name == username
+        return user
+      end
+    end
+    false
+  end
+
+  def find_merchant_by_name(merchant_name)
+    @payback_service.merchants.each do |merchant|
+      if merchant.name == merchant_name
+        return merchant
+      end
+    end
+    false
+  end
+
 end
--- spec/command_parser_spec.rb
+++ spec/command_parser_spec.rb
@@ -3,10 +3,39 @@ describe CommandParser do
   before :each do
     @cmd_parser = CommandParser.new
   end
-  context "parsing 'new' command'" do
-    it "should be able to parse 'new user user1 u1@users.com 300'" do
+  context "creating new users and merchants 'new' command" do
+    it "should be able to onboard new user by parsing 'new user user1 u1@users.com 300'" do
       input = 'new user user1 u1@users.com 300'
       expect(@cmd_parser.parse_new_command(input)).to eql('user1 (300)')
+    end
+    it "should be able to onboard new merchant by parsing 'new merchant m1 m1@merchants.com 0.5%'" do
+      input = 'new merchant m1 m1@merchants.com 0.5%'
+      expect(@cmd_parser.parse_new_command(input)).to eql('m1 (0.5%)')
+    end
+  end
+  context "parsing command line with state" do
+    it "Parse and report transactions, users-at-credit-limit, discounts and payback with state management" do
+      # The example pointed out in the PDF is used as the input here
+      input = 'new user user1 u1@users.com 300'
+      expect(@cmd_parser.parse(input)).to eql('user1 (300)')
+
+      input = 'new user user2 u2@users.com 400'
+      expect(@cmd_parser.parse(input)).to eql('user2 (400)')
+
+      input = 'new user user3 u3@users.com 500'
+      expect(@cmd_parser.parse(input)).to eql('user3 (500)')
+
+      input = 'new merchant m1 m1@merchants.com 0.5%'
+      expect(@cmd_parser.parse(input)).to eql('m1 (0.5%)')
+
+      input = 'new merchant m2 m2@merchants.com 1.5%'
+      expect(@cmd_parser.parse(input)).to eql('m2 (1.5%)')
+
+      input = 'new merchant m3 m3@merchants.com 1.25%'
+      expect(@cmd_parser.parse(input)).to eql('m3 (1.25%)')
+
+      input = 'new txn user2 m1 500'
+      expect(@cmd_parser.parse(input)).to eql('rejected! (reason: credit limit)')
     end
   end
 end
